87575a0905c2cd09e303036decdb821f54033719
chriscle2013 <chriscle2013@gmail.com>
2025-11-10 18:20:59 -0500

    Modify save_lead function to include email and business

--- db.py
+++ db.py
@@ -7,17 +7,18 @@ def get_connection():
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASS"),
         host=os.getenv("DB_HOST"),
-        port="5432"
+        port="5432",
+        sslmode="require"
     )
     return conn
 
-def save_lead(name, platform, duration, goal, tone):
+def save_lead(name, email, business, platform, duration, goal, tone):
     conn = get_connection()
     cur = conn.cursor()
     cur.execute("""
-        INSERT INTO leads (name, platform, duration, goal, tone)
-        VALUES (%s, %s, %s, %s, %s)
-    """, (name, platform, duration, goal, tone))
+        INSERT INTO leads (name, email, business, platform, duration, goal, tone)
+        VALUES (%s, %s, %s, %s, %s, %s, %s)
+    """, (name, email, business, platform, duration, goal, tone))
     conn.commit()
     cur.close()
     conn.close()
